22c22c22bb528be37da0a4e00fba861bd70651e8
aaronyu <ayu@workfusion.com>
2018-09-19 17:28:11 -0400

    Leetcode - HashTable

---
+++ LeetCode/src/HashTable/L409_LongestPalindrome.java
@@ -0,0 +1,29 @@
+package HashTable;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class L409_LongestPalindrome {
+
+    public static int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                count++;
+                set.remove(c);
+            } else {
+                set.add(c);
+            }
+        }
+
+        return (!set.isEmpty()? count*2 + 1: count * 2);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("daefvafvfwesdfaef"));
+    }
+}
