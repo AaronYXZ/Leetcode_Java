a4506db106285478aa04ac955aa5e1d9e8737bb7
aaronyu <ayu@workfusion.com>
2018-09-25 15:24:28 -0400

    Leetcode - LinkedList

--- LeetCode/src/LinkedList/L21_MergeTwoLists.java
+++ LeetCode/src/LinkedList/L21_MergeTwoLists.java
@@ -19,10 +19,12 @@ https://leetcode.com/problems/merge-two-sorted-lists/discuss/9715/Java-1-ms-4-li
 public class L21_MergeTwoLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null)
+        if (l1 == null) {
             return l2;
-        if (l2 == null)
+        }
+        if (l2 == null) {
             return l1;
+        }
         if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
