7cd8652f776c755c97136e5f98a6e142b295a53b
aaronyu <ayu@workfusion.com>
2018-09-30 21:37:57 -0400

    CS61B - Chap 3 Testing

--- CS61B/src/C3_Testing/TestSort.java
+++ CS61B/src/C3_Testing/TestSort.java
@@ -1,15 +1,16 @@
 package C3_Testing;
 
-import java.util.Arrays;
+
+import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
-import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
 
 
 public class TestSort {
 
-    public static void testSort() {
+    @Test
+    public void testSort() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "i"};
         String[] input2 = {"an", "egg", "have", "i"};
@@ -19,25 +20,42 @@ public class TestSort {
         //        if (input != expected){ // abuse of !=
         //            System.out.println("Error Sort");
         //        }
-        if(!Arrays.equals(input,  expected)){
-            System.out.println("Sort Error!");
-        }
-//        org.junit.Assert.assertArrayEquals(input, expected);
+        //        if(!Arrays.equals(input,  expected)){
+        //            System.out.println("Sort Error!");
+        //        }
+        //        org.junit.Assert.assertArrayEquals(input, expected);
         assertArrayEquals(input, expected);
 
     }
 
     // Test the Sort.findSmallest Method
-    public static void testFindSmallest(){
+    @Test
+    public void testFindSmallest() {
         String[] input = {"i", "have", "an", "egg"};
-        String expected = "an";
-
-        String actual = Sort.findSmallest(input);
+        int expected = 2;
+        int actual = Sort.findSmallest(input, 1);
         assertEquals(expected, actual);
 
+        String[] input2 = {"there", "are", "many", "pigs"};
+        int expected2 = 3;
+
+        int actual2 = Sort.findSmallest(input2, 3);
+        assertEquals(expected2, actual2);
+
     }
 
-    public static void main(String[] args) {
-        testFindSmallest();
+    @Test
+    public void testSwap() {
+        String[] input = {"i", "have", "an", "egg"};
+        Sort.swap(input, 1, 3);
+        String[] expected = {"i", "egg", "an", "have"};
+        assertArrayEquals(input, expected);
+
     }
+
+    //    public static void main(String[] args) {
+    ////        testFindSmallest();
+    ////        testSwap();
+    //        testSort();
+    //    }
 }
