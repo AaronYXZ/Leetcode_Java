6be23c90eb679699c3aea187e56787051c8614a8
aaronyu <ayu@workfusion.com>
2018-10-07 12:28:52 -0400

    Leetcode - LinkedList

---
+++ LeetCode/src/LinkedList/L138_CopyListWithRandomPointer.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class L138_CopyListWithRandomPointer {
+
+    public class RandomListNode {
+
+        int label;
+        RandomListNode next, random;
+
+        RandomListNode(int x) { this.label = x; }
+    }
+
+    public RandomListNode copyRandomList(RandomListNode head) {
+
+        RandomListNode iter = head, next;
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+        while(iter != null){
+            next = iter.next;
+
+            RandomListNode copy = new RandomListNode(iter.label);
+            iter.next = copy;
+            copy.next = next;
+
+            iter = next;
+        }
+
+    }
+}
--- LeetCode/src/LinkedList/L148_SortList.java
+++ LeetCode/src/LinkedList/L148_SortList.java
@@ -1,5 +1,25 @@
 package LinkedList;
 
+/*
+Q:
+https://leetcode.com/problems/sort-list/description/
+
+Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+
+
+S:
+https://leetcode.com/problems/sort-list/discuss/46714/Java-merge-sort-solution
+ */
+
 public class L148_SortList {
     public ListNode sortList(ListNode head){
         if (head == null || head.next == null) return head;
