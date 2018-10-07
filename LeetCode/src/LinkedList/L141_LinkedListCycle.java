0e838942b08dc5125af8c5a9acf3129234ddee13
aaronyu <ayu@workfusion.com>
2018-10-06 23:39:21 -0400

    Leetcode - LinkedList

--- LeetCode/src/LinkedList/L141_LinkedListCycle.java
+++ LeetCode/src/LinkedList/L141_LinkedListCycle.java
@@ -1,5 +1,18 @@
 package LinkedList;
 
+/*
+Q:
+https://leetcode.com/problems/linked-list-cycle/description/
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+S:
+https://leetcode.com/problems/linked-list-cycle/discuss/44489/O(1)-Space-Solution
+ */
+
 public class L141_LinkedListCycle {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
