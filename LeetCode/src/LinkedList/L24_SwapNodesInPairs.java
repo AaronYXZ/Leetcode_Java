0d8abc222cabd02de5edec0f688d888e6d8cb503
aaronyu <ayu@workfusion.com>
2018-10-07 22:13:33 -0400

    Leetcode - LinkedList

--- LeetCode/src/LinkedList/L24_SwapNodesInPairs.java
+++ LeetCode/src/LinkedList/L24_SwapNodesInPairs.java
@@ -1,5 +1,23 @@
 package LinkedList;
 
+/*
+Q:
+https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+Note:
+
+Your algorithm should use only constant extra space.
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+S:
+https://leetcode.com/problems/swap-nodes-in-pairs/discuss/11046/My-simple-JAVA-solution-for-share
+ */
+
 public class L24_SwapNodesInPairs {
 
     public ListNode swapPairs(ListNode head){
