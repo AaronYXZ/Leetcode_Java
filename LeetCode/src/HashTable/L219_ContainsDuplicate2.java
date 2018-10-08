d91ee4065ecabfc5459bf7f99d87efe1c6cadbea
aaronyu <ayu@workfusion.com>
2018-10-07 22:10:07 -0400

    Leetcode - LinkedList

--- LeetCode/src/HashTable/L219_ContainsDuplicate2.java
+++ LeetCode/src/HashTable/L219_ContainsDuplicate2.java
@@ -28,13 +28,17 @@ Sliding Window
  */
 
 public class L219_ContainsDuplicate2 {
-    public boolean solution1(int[] nums, int k){
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i< nums.length; i++){
-            if (i > k) set.remove(nums[i -k -1]); // Sliding Window
-            if(!set.add(nums[i])) return true;
-        }
 
+    public boolean solution1(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
         return false;
     }
 
