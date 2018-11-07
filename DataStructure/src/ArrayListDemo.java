67a85565a41f4dc0576cc395bfaa616774c7db41
aaronyu <ayu@workfusion.com>
2018-11-07 12:43:41 -0500

    DataStructure - ArrayList

---
+++ DataStructure/src/ArrayListDemo.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        // initialize an ArrayList
+        List<String> obj = new ArrayList<>();
+
+        /*This is how we add elements to an ArrayList*/
+        obj.add("Ajeet");
+        obj.add("Harry");
+        obj.add("Chaitanya");
+        obj.add("Steve");
+        obj.add("Anuj");
+
+        for (String string:obj){
+            System.out.println(string);
+        }
+
+        /* Add element at the given index
+         * obj.add(0, "Rahul") - Adding element "Rahul" at first position
+         * obj.add(3, "Justin") - Adding element "Justin" at fourth position
+         */
+        obj.add(0, "Rahul");
+        obj.add(3, "Justin");
+
+        for (int i = 0; i < obj.size(); i++){
+            System.out.println(obj.get(i));
+        }
+
+        //Remove elements from ArrayList like this
+        obj.remove("Chaitanya"); //Removes "Chaitanya" from ArrayList
+        obj.remove("Harry"); //Removes "Harry" from ArrayList
+    }
+}
---
+++ DataStructure/src/ArrayListInitDemo.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListInitDemo {
+
+    public static void main(String[] args) {
+        // 1.use Arrays.asList
+        // can't add new items afterwards
+        List<String> obj = Arrays.asList("a", "b", "c");
+//        obj.add("d");
+        for (String string:obj){
+            System.out.println(string);
+        }
+
+        // 2.Normal way of using add
+        List<String> obj2 = new ArrayList<>();
+        obj2.add("a");
+        obj2.add("b");
+        obj2.add("c");
+
+    }
+}
