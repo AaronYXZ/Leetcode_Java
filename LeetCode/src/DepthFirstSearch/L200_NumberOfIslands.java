93403f1f9377ab73863c085e446a7c445906f6fb
aaronyu <ayu@workfusion.com>
2018-11-18 18:33:33 -0500

    Leetcode - Graph

--- LeetCode/src/DepthFirstSearch/L200_NumberOfIslands.java
+++ LeetCode/src/DepthFirstSearch/L200_NumberOfIslands.java
@@ -1,30 +1,67 @@
 package DepthFirstSearch;
 
-public class L200_NumberOfIslands {
-    private int n;
-    private int m;
+/*
+Q:
+https://leetcode.com/problems/number-of-islands/description/
 
-    public int numIslands(char[][] grid) {
-        int count = 0;
-        n = grid.length;
-        if (n == 0) return 0;
-        m = grid[0].length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++)
-                if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
-                    ++count;
-                }
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
+
+S:
+https://leetcode.com/problems/number-of-islands/discuss/56359/Very-concise-Java-AC-solution
+ */
+
+public class L200_NumberOfIslands {
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
         }
-        return count;
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
     }
 
-    private void DFSMarking(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
-        grid[i][j] = '0';
-        DFSMarking(grid, i + 1, j);
-        DFSMarking(grid, i - 1, j);
-        DFSMarking(grid, i, j + 1);
-        DFSMarking(grid, i, j - 1);
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
     }
 }
