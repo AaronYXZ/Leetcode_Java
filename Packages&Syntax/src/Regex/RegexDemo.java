09fd503c4e385371782d1ade0067a2d59e028642
aaronyu <ayu@workfusion.com>
2018-11-29 13:08:23 -0500

    Leetcode

---
+++ Packages&Syntax/src/Regex/RegexDemo.java
@@ -0,0 +1,21 @@
+package Regex;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo {
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("\\d{1,3}");
+        String text = "123 is all I have except for1, 2 and 3; I don't have 12, 23 or 13";
+        Set<String> matched = new HashSet<>();
+
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()){
+            matched.add(matcher.group());
+        }
+        matched.stream().forEach(System.out::println);
+    }
+}
