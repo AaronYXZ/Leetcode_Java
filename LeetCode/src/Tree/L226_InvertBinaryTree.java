b9468f8a06140514477787c680f719b1e217233d
aaronyu <ayu@workfusion.com>
2018-12-17 13:59:56 -0500

    Leetcode - Tree

---
+++ LeetCode/src/Tree/L226_InvertBinaryTree.java
@@ -0,0 +1,33 @@
+package Tree;
+
+/*
+Q:
+https://leetcode.com/problems/invert-binary-tree/description/
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+S:
+https://leetcode.com/problems/invert-binary-tree/discuss/62707/Straightforward-DFS-recursive-iterative-BFS-solutions
+ */
+
+public class L226_InvertBinaryTree {
+
+
+}
