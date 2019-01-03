5983860b13dd3068fb9476465d70cfb17c5eb888
aaronyu <ayu@workfusion.com>
2019-01-02 20:32:16 -0500

    Leetcode - Tree

--- LeetCode/src/Tree/L102_BinaryTreeLevelOrderTraversal.java
+++ LeetCode/src/Tree/L102_BinaryTreeLevelOrderTraversal.java
@@ -4,6 +4,29 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/*
+https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+S:
+https://leetcode.com/problems/binary-tree-level-order-traversal/discuss/33450/Java-solution-with-a-queue-used
+ */
+
 public class L102_BinaryTreeLevelOrderTraversal {
 
     public List<List<Integer>> levelOrder(TreeNode root){
