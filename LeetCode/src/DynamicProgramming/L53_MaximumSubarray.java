eeec4f134bdf65234c4314df1da9910c9bd967a5
aaronyu <ayu@workfusion.com>
2019-01-05 11:53:33 -0500

    Leetcode - Dynamic Programming

--- LeetCode/src/DynamicProgramming/L53_MaximumSubarray.java
+++ LeetCode/src/DynamicProgramming/L53_MaximumSubarray.java
@@ -1,5 +1,14 @@
 package DynamicProgramming;
 
 public class L53_MaximumSubarray {
+    public static int maxSubArray(int[] nums){
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
 
+        for (int i = 1; i<nums.length; i++){
+            maxEndingHere = Math.max(maxEndingHere+nums[i], nums[i]);
+            maxSoFar = Math.max(maxEndingHere, maxSoFar);
+        }
+
+        return maxSoFar;
+    }
 }
