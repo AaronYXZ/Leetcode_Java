5f87fe3f197cf8bdaad1d536c497f16db525f25e
aaronyu <ayu@workfusion.com>
2019-01-05 17:04:39 -0500

    Leetcode - Dynamic Programming

--- LeetCode/src/DynamicProgramming/L121_BestTimetoBuyandSellStock.java
+++ LeetCode/src/DynamicProgramming/L121_BestTimetoBuyandSellStock.java
@@ -1,13 +1,46 @@
 package DynamicProgramming;
 
+/*
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+S:
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-:)-(In-case-if-interviewer-twists-the-input)
+ */
+
 public class L121_BestTimetoBuyandSellStock {
 
-    public int maxProfit(int[] prices) {
+    public static int maxProfit(int[] prices) {
         int maxCur = 0, maxSoFar = 0;
-        for(int i = 1; i < prices.length; i++) {
-            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+        for (int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur + prices[i] - prices[i - 1]);
+            // compare to L53_MaxiumSubarray
+            // maxCur = Math.max(nums[i] , maxCur + nums[i])
+            // nums[i] is prices[i] - prices[i-1], the first is 0 instead of nums[i] cause if diff is negative, then we dont buy and sell stock
             maxSoFar = Math.max(maxCur, maxSoFar);
         }
         return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1, 3, 5, 9};
+        System.out.println(maxProfit(prices));
     }
 }
