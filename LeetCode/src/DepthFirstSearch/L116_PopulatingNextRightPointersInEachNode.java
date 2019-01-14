38fca9c57589872d2ce8857dff3dd7156221c8c9
aaronyu <ayu@workfusion.com>
2019-01-14 17:11:51 -0500

    Leetcode - Tree / DFS / BFS

---
+++ LeetCode/src/DepthFirstSearch/L116_PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,61 @@
+package DepthFirstSearch;
+
+/*
+Q:
+https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+
+Given a binary tree
+
+private class TreeLinkNode{
+    TreeLinkNode left, right, next;
+    int val;
+}
+
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+Example:
+
+Given the following perfect binary tree,
+
+     1
+   /  \
+  2    3
+ / \  / \
+4  5  6  7
+After calling your function, the tree should look like:
+
+     1 -> NULL
+   /  \
+  2 -> 3 -> NULL
+ / \  / \
+4->5->6->7 -> NULL
+
+S:
+https://leetcode.com/problems/populating-next-right-pointers-in-each-node/discuss/37461/Java-solution-with-O(1)-memory+-O(n)-time
+ */
+
+public class L116_PopulatingNextRightPointersInEachNode {
+    private class TreeLinkNode{
+        TreeLinkNode left, right, next;
+        int val;
+    }
+    public void connect(TreeLinkNode root){
+        TreeLinkNode level_start = root;
+        while (level_start != null){
+            TreeLinkNode curr = level_start;
+            while(curr != null){
+                if(curr.left != null) curr.left.next = curr.right;
+                if(curr.right!= null && curr.next != null) curr.right.next = curr.next.left;
+                curr = curr.next;
+            }
+            level_start = level_start.left;
+        }
+    }
+}
--- LeetCode/src/String/L680_ValidPalindromeII.java
+++ LeetCode/src/String/L680_ValidPalindromeII.java
@@ -6,9 +6,10 @@ S:
  */
 
 public class L680_ValidPalindromeII {
-    public static boolean solution1(String s){
 
-        if (s == null || s.length() == 0){
+    public static boolean solution1(String s) {
+
+        if (s == null || s.length() == 0) {
             return true;
         }
 
@@ -17,21 +18,52 @@ public class L680_ValidPalindromeII {
         int front = 0;
         int end = s.length() - 1;
 
-        while (front < end){
+        while (front < end) {
             char frontChar = s.charAt(front++);
             char endChar = s.charAt(end++);
             if (frontChar != endChar) {
                 if (flag) {
                     flag = false;
-                    if (s.charAt(front + 1) == endChar)
+                    if (s.charAt(front + 1) == endChar) {
                         front++;
-                    if (s.charAt(end - 1) == frontChar)
+                    }
+                    if (s.charAt(end - 1) == frontChar) {
                         end--;
+                    }
+                } else {
+                    break;
                 }
-                else break;
             }
 
         }
         return front >= end;
     }
+
+    public static boolean vP(String s) {
+        int record = 0;
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } else if (s.charAt(l + 1) == s.charAt(r)) {
+                record++;
+                l += 2;
+                r--;
+            } else if (s.charAt(l) == s.charAt(r - 1)) {
+                record++;
+                r -= 2;
+                l++;
+            } else {
+                return false;
+            }
+        }
+        return record <= 1;
+    }
+
+    public static void main(String[] args) {
+        String test = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+        System.out.println(vP(test));
+    }
+
 }
