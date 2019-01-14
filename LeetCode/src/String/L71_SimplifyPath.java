cf23d29383f4e562d559292122c1a9f0fec7cc77
aaronyu <ayu@workfusion.com>
2019-01-13 21:22:42 -0500

    Leetcode - String

--- LeetCode/src/String/L71_SimplifyPath.java
+++ LeetCode/src/String/L71_SimplifyPath.java
@@ -1,41 +1,30 @@
 package String;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
 /*
 Q:
 https://leetcode.com/problems/simplify-path/description/
 S:
-http://www.cnblogs.com/grandyang/p/4347125.html
+https://leetcode.com/problems/simplify-path/discuss/25686/Java-10-lines-solution-with-stack
 
-Given an absolute path for a file (Unix-style), simplify it.
-
-For example,
-path = "/home/", => "/home"
-path = "/a/./b/../../c/", => "/c"
-
-Corner Cases:
-
-Did you consider the case where path = "/../"?
-In this case, you should return "/".
-Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
-In this case, you should ignore redundant slashes and return "/home/foo".
+The main idea is to push to the stack every valid file name (not in {"",".",".."}), popping only if there's smth to pop and we met "..".
  */
 
 public class L71_SimplifyPath {
-    public static String solution2(String path){
-
-        Stack<String> strings = new Stack<>();
-        String[] p = path.split("/");
-        for (String t:p){
-            if (!t.isEmpty() && t.equals("..")) strings.pop();
-            else if (!t.equals(".") && !t.equals("") && !t.equals("..")) strings.push(t);
+    public String simplifyPath(String path) {
+        Deque<String> stack = new LinkedList<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
+            else if (!skip.contains(dir)) stack.push(dir);
         }
-
-        List<String> list = new ArrayList<>(strings);
-        return "/" + String.join("/", list);
-
+        String res = "";
+        for (String dir : stack) res = "/" + dir + res;
+        return res.isEmpty() ? "/" : res;
     }
 }
