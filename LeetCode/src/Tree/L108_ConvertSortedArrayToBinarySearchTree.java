0d20f9c5b1e0738538d7e28bbe5c43f92b576440
aaronyu <ayu@workfusion.com>
2019-01-15 14:05:42 -0500

    Leetcode - Tree / DFS / BFS

--- LeetCode/src/Tree/L108_ConvertSortedArrayToBinarySearchTree.java
+++ LeetCode/src/Tree/L108_ConvertSortedArrayToBinarySearchTree.java
@@ -22,24 +22,31 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
 
  S:
  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/discuss/35220/My-Accepted-Java-Solution
+ 这道题是二分查找树的题目，要把一个有序数组转换成一颗二分查找树。其实从本质来看，如果把一个数组看成一棵树（也就是以中点为根，左右为左右子树，依次下去）数组就等价于一个二分查找树。所以如果要构造这棵树，那就是把中间元素转化为根，然后递归构造左右子树。所以我们还是用二叉树递归的方法来实现，以根作为返回值，每层递归函数取中间元素，作为当前根和赋上结点值，然后左右结点接上左右区间的递归函数返回值。时间复杂度还是一次树遍历O(n)，总的空间复杂度是栈空间O(logn)加上结果的空间O(n)，额外空间是O(logn)，总体是O(n)。
+---------------------
+原文：https://blog.csdn.net/linhuanmars/article/details/23904883
  */
 
 public class L108_ConvertSortedArrayToBinarySearchTree {
 
-    public TreeNode sortedArrayToBST(int[] num){
-        if (num == null || num.length == 0) return null;
+    public TreeNode sortedArrayToBST(int[] num) {
+        if (num == null || num.length == 0) {
+            return null;
+        }
 
-        TreeNode head = helper(num, 0, num.length-1);
+        TreeNode head = helper(num, 0, num.length - 1);
         return head;
     }
 
-    public TreeNode helper(int[] num, int low, int high){
-        if (high < low) return null;
+    public TreeNode helper(int[] num, int low, int high) {
+        if (high < low) {
+            return null;
+        }
 
         int mid = (low + high) / 2;
         TreeNode node = new TreeNode(num[mid]);
         node.left = helper(num, low, mid - 1);
-        node.right = helper(num, mid + 1 , high);
+        node.right = helper(num, mid + 1, high);
         return node;
     }
 }
