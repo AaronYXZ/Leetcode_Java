34c95fc1ff883823864bb51720119067d503fe14
aaronyu <ayu@workfusion.com>
2019-01-15 14:32:25 -0500

    Leetcode - Tree / DFS / BFS

---
+++ LeetCode/src/Tree/L111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+package Tree;
+/*
+https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its minimum depth = 2.
+
+https://leetcode.com/problems/minimum-depth-of-binary-tree/discuss/36045/My-4-Line-java-solution
+ */
+
+public class L111_MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+    }
+}
