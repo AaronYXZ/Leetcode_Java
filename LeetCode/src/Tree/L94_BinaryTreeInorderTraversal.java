98bb7a183aed13fc928bf2aebe2e51ac595553eb
aaronyu <ayu@workfusion.com>
2019-01-14 19:51:02 -0500

    Leetcode - Tree / DFS / BFS

---
+++ LeetCode/src/Tree/L94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,54 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+/*
+https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+S:
+https://leetcode.com/problems/validate-binary-search-tree/discuss/32112/Learn-one-iterative-inorder-traversal-apply-it-to-multiple-tree-questions-(Java-Solution)
+ */
+public class L94_BinaryTreeInorderTraversal {
+    public static List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.empty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        List<Integer> result = inorderTraversal(root);
+    }
+
+}
