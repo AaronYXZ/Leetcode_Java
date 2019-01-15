d31cc1eb074427aba7d812cc9f11742d5dd4e1be
aaronyu <ayu@workfusion.com>
2019-01-14 20:10:42 -0500

    Leetcode - Tree / DFS / BFS

---
+++ LeetCode/src/Tree/L230_KthSmallestElementInBST.java
@@ -0,0 +1,61 @@
+package Tree;
+
+import java.util.Stack;
+/*
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+S:
+https://leetcode.com/problems/validate-binary-search-tree/discuss/32112/Learn-one-iterative-inorder-traversal-apply-it-to-multiple-tree-questions-(Java-Solution)
+ */
+
+public class L230_KthSmallestElementInBST {
+    public static int kthSmallest(TreeNode root, int k){
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.empty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) break;
+            root = root.right;
+        }
+        return root.val;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        root.right = new TreeNode(4);
+        System.out.println(kthSmallest(root, 3));
+    }
+}
