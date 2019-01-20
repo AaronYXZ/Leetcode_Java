ff342a1847beea1306793d186456a3f2cd23047a
aaronyu <ayu@workfusion.com>
2019-01-20 14:07:50 -0500

    Leetcode - Tree / DFS / BFS

---
+++ LeetCode/src/Array/L189_RotateArray.java
@@ -0,0 +1,48 @@
+package Array;
+
+/*
+https://leetcode.com/problems/rotate-array/description/
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+Note:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+
+
+https://leetcode.com/problems/rotate-array/discuss/54250/Easy-to-read-Java-solution
+ */
+
+public class L189_RotateArray {
+    public void rotate(int[] nums, int k){
+        int j = k % nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+    public void reverse(int[] nums, int start, int end){
+        while (start < end){
+            int temp = nums[end];
+            nums[end]  = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
