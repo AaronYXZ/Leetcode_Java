d28fe23f3a1e3c14af8f945db55f5a3da2ef9314
aaronyu <ayu@workfusion.com>
2019-01-25 20:01:58 -0500

    Tree / DFS / BFS

---
+++ LeetCode/src/Tree/L255_VerifyPreorderSequenceinBST.java
@@ -0,0 +1,16 @@
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class L255_VerifyPreorderSequenceinBST {
+    public boolean verifyPreorder(int[] preorder){
+        int low = Integer.MIN_VALUE;
+        Stack<Integer> path = new Stack<>();
+        Queue<Integer> path2 = new LinkedList<>();
+//        Stack<Integer> path3 = new LinkedList<>();
+        return false;
+
+    }
+}
