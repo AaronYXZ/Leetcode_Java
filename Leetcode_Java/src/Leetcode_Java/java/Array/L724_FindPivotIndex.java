95e82b706bec27537acbd81e31258dab7f588298
AaronDat <arswalcott@163.com>
2019-02-13 20:34:38 -0500

    Leetcode - Array

---
+++ Leetcode_Java/src/Leetcode_Java/java/Array/L724_FindPivotIndex.java
@@ -0,0 +1,54 @@
+package Array;
+
+/*
+https://leetcode.com/problems/find-pivot-index/description/
+Given an array of integers nums, write a method that returns the "pivot" index of this array.
+
+We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
+
+If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
+
+Example 1:
+
+Input:
+nums = [1, 7, 3, 6, 5, 6]
+Output: 3
+Explanation:
+The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers to the right of index 3.
+Also, 3 is the first index where this occurs.
+
+
+Example 2:
+
+Input:
+nums = [1, 2, 3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+
+
+Note:
+
+The length of nums will be in the range [0, 10000].
+Each element nums[i] will be an integer in the range [-1000, 1000].
+ */
+public class L724_FindPivotIndex {
+    // pay attention to corner cases.
+    // length = 1, then idx is 0
+    // idx = 0 or length-1, the sum of the other side is considered as 0
+    public int pivotIndex(int[] nums) {
+        if (nums.length == 1) return 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++){
+            sum+=nums[i];
+        }
+        int leftSum = 0;
+        for (int idx = 0; idx < nums.length; idx++){
+            if ( leftSum * 2 + nums[idx] == sum){
+                return idx;
+            }
+            leftSum+=nums[idx];
+        }
+        return -1;
+    }
+}
