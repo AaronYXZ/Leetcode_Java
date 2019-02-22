136011dc57ab220e08bc6b600ecf81aefcf243d4
aaronyu <ayu@workfusion.com>
2019-02-21 20:59:02 -0500

    Tree

--- Leetcode_Java/src/Leetcode_Java/java/Tree/L366_FindLeavesOfBinaryTree.java
+++ Leetcode_Java/src/Leetcode_Java/java/Tree/L366_FindLeavesOfBinaryTree.java
@@ -8,4 +8,13 @@ public class L366_FindLeavesOfBinaryTree {
         List<List<Integer>> result = new ArrayList<>();
         return result;
     }
+    private int height(TreeNode node,  List<List<Integer>> result){
+        if (null == node) return -1;
+        int level = 1 + Math.max(height(node.left, result), height(node.right, result));
+        if (result.size() < level + 1){
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(node.val);
+        return level;
+    }
 }
