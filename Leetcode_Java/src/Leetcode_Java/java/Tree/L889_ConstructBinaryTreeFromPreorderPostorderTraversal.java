f91af2dd00574c307dfa08a9b8655f244ea60245
aaronyu <ayu@workfusion.com>
2019-02-23 21:30:43 -0500

    Tree

---
+++ Leetcode_Java/src/Leetcode_Java/java/Tree/L889_ConstructBinaryTreeFromPreorderPostorderTraversal.java
@@ -0,0 +1,42 @@
+package Tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/description/
+Return any binary tree that matches the given preorder and postorder traversals.
+
+Values in the traversals pre and post are distinct positive integers.
+
+
+
+Example 1:
+
+Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
+
+
+Note:
+
+1 <= pre.length == post.length <= 30
+pre[] and post[] are both permutations of 1, 2, ..., pre.length.
+It is guaranteed an answer exists. If there exists multiple answers, you can return any of them.
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/discuss/161268/C++JavaPython-One-Pass-Real-O(N)
+ */
+public class L889_ConstructBinaryTreeFromPreorderPostorderTraversal {
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        Deque<TreeNode> s = new ArrayDeque<>();
+        s.offer(new TreeNode(pre[0]));
+        for (int i = 1, j = 0; i < pre.length; ++i) {
+            TreeNode node = new TreeNode(pre[i]);
+            while (s.getLast().val == post[j]) {
+                s.pollLast(); j++;
+            }
+            if (s.getLast().left == null) s.getLast().left = node;
+            else s.getLast().right = node;
+            s.offer(node);
+        }
+        return s.getFirst();
+    }
+}
