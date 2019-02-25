b09c89f469c5a0b0c05a7ccd834545484bc5cf52
aaronyu <ayu@workfusion.com>
2019-02-25 13:54:19 -0500

    Backtracking

---
+++ Leetcode_Java/src/Leetcode_Java/java/Backtracking/L39_CombinationSum.java
@@ -0,0 +1,60 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+https://leetcode.com/problems/combination-sum/description/
+
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+The same repeated number may be chosen from candidates unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+Example 2:
+
+Input: candidates = [2,3,5], target = 8,
+A solution set is:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+
+https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
+ */
+
+public class L39_CombinationSum {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(list, new ArrayList<>(), candidates, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                tempList.add(candidates[i]);
+                backtrack(result, tempList, candidates, remain - candidates[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
--- Leetcode_Java/src/Leetcode_Java/java/String/L890_FindAndReplacePattern.java
+++ Leetcode_Java/src/Leetcode_Java/java/String/L890_FindAndReplacePattern.java
@@ -4,35 +4,77 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+/*
+https://leetcode.com/problems/find-and-replace-pattern/description/
+
+You have a list of words and a pattern, and you want to know which words in words matches the pattern.
+
+A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we get the desired word.
+
+(Recall that a permutation of letters is a bijection from letters to letters: every letter maps to another letter, and no two letters map to the same letter.)
+
+Return a list of the words in words that match the given pattern.
+
+You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+Output: ["mee","aqq"]
+Explanation: "mee" matches the pattern because there is a permutation {a -> m, b -> e, ...}.
+"ccc" does not match the pattern because {a -> c, b -> c, ...} is not a permutation,
+since a and b map to the same letter.
+
+
+Note:
+
+1 <= words.length <= 50
+1 <= pattern.length = words[i].length <= 20
+
+ */
 
 public class L890_FindAndReplacePattern {
-    public static List<String> findAndReplacePattern(String[] words, String pattern){
+
+    public static List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> result = new ArrayList<>();
         int pl = pattern.length();
-        for (String word : words){
-            if (word.length() != pl) continue;
+        for (String word : words) {
+            if (word.length() != pl) {
+                continue;
+            }
             Map<Character, Character> map = new HashMap<>();
             boolean flag = true;
-            for (int i  = 0; i < pl; i++){
-               char p = pattern.charAt(i);
-               char w = word.charAt(i);
-               if (!map.containsKey(p)){
-                   map.put(p, w);
-               } else if (map.get(p) != w){
-                   flag = false;
-               }
+            for (int i = 0; i < pl; i++) {
+                char p = pattern.charAt(i);
+                char w = word.charAt(i);
+                // check if the char from p already exists
+                if (!map.containsKey(p)) { // p doesn't exist, then check if value from word already exists
+                    if (map.containsValue(w)) {
+                        flag = false;
+                        continue;
+                    } else {
+                        map.put(p, w);
+                    }
+                } else if (map.get(p) != w) { // p already exist, get the value from map and compare from char from word
+                    flag = false;
+                    continue;
+                }
             }
-            if (flag) result.add(word);
+            if (flag) {
+                result.add(word);
+            }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        String[] words = new String[]{"abc","deq","mee","aqq","dkd","ccc"};
+        String[] words = new String[] {"abc", "deq", "mee", "aqq", "dkd", "ccc"};
         String pattern = "abb";
         List<String> result;
         result = findAndReplacePattern(words, pattern);
-        System.out.println("a");
+        System.out.println(result);
 
     }
 }
