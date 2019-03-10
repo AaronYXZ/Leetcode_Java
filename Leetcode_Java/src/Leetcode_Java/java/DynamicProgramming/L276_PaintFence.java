9fe34dd8007e432e5336212ca9287cc1b611710b
aaronyu <ayu@workfusion.com>
2019-03-10 12:29:42 -0400

    Dynamic Programming

---
+++ Leetcode_Java/src/Leetcode_Java/java/DynamicProgramming/L276_PaintFence.java
@@ -0,0 +1,44 @@
+package DynamicProgramming;
+/*
+https://leetcode.com/problems/paint-fence/
+There is a fence with n posts, each post can be painted with one of the k colors.
+
+You have to paint all the posts such that no more than two adjacent fence posts have the same color.
+
+Return the total number of ways you can paint the fence.
+
+Note:
+n and k are non-negative integers.
+
+Example:
+
+Input: n = 3, k = 2
+Output: 6
+Explanation: Take c1 as color 1, c2 as color 2. All possible ways are:
+
+            post1  post2  post3
+ -----      -----  -----  -----
+   1         c1     c1     c2
+   2         c1     c2     c1
+   3         c1     c2     c2
+   4         c2     c1     c1
+   5         c2     c1     c2
+   6         c2     c2     c1
+
+  https://leetcode.com/problems/paint-fence/discuss/71156/O(n)-time-java-solution-O(1)-space
+ */
+
+public class L276_PaintFence {
+    public int numWays(int n, int k){
+        if (n == 0) return 0;
+        if (n == 1) return k;
+        int same = k;
+        int diff = k * (k - 1);
+        for (int i = 2; i < n; i++){
+            int tmp = diff;
+            diff = (diff + same) * (k -1);
+            same = tmp ;
+        }
+        return same + diff;
+    }
+}
